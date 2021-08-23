cb65a0b6fc7d68f62fb07e3cc00202c84d104792
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2021-08-23 13:20:59 +0800

    able to run preprocess

---
+++ analytical_engine/preprocess.sh
@@ -0,0 +1,16 @@
+DIR=$(pushd $(dirname $BASH_SOURCE[0]) > /dev/null && pwd && popd > /dev/null)
+
+M2_REPO_GRAPE=~/.m2/repository/com/alibaba/grape
+
+grape_demo_jar=${M2_REPO_GRAPE}/grape-demo/0.1/grape-demo-0.1-jar-with-dependencies.jar
+grape_sdk_jar=${M2_REPO_GRAPE}/grape-sdk/0.1/grape-sdk-0.1-jar-with-dependencies.jar
+grape_processor_jar=${M2_REPO_GRAPE}/grape-processor/0.1/grape-processor-0.1-jar-with-dependencies.jar
+
+PRE_CP=${grape_demo_jar}:${grape_sdk_jar}:${grape_processor_jar}
+JVM_OPTS="-Djava.class.path=${PRE_CP}"
+
+task_main_class=com.alibaba.grape.TraverseMainClass
+${DIR}/build/run_java_app_preprocess \
+                        ${task_main_class} \
+                        ${grape_demo_jar} \
+                        /tmp/demo.properties vfile efile 
