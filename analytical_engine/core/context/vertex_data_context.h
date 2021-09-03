3ad31d8232580df657eb45eaad35fddd55dde2a3
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2021-09-03 16:24:55 +0800

    fix getvalue

--- analytical_engine/core/context/vertex_data_context.h
+++ analytical_engine/core/context/vertex_data_context.h
@@ -153,9 +153,9 @@ class LabeledVertexDataContext : public grape::ContextBase {
 
   const fragment_t& fragment() { return fragment_; }
 
-  const data_t& GetValue(vertex_t v) const {
-    label_id_t i = fragment_->vertex_label(v);
-    int64_t offset = fragment_->vertex_offset(v);
+  const data_t& GetValue(vertex_t& v) const {
+    label_id_t i = fragment_.vertex_label(v);
+    int64_t offset = fragment_.vertex_offset(v);
     return data_[i][offset];
   }
 
