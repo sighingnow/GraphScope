9104401b6d2fab705216e492ccd223f7fcfe60b8
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2021-09-06 19:52:41 +0800

    template method cannot diff

--- analytical_engine/core/parallel/property_message_manager.h
+++ analytical_engine/core/parallel/property_message_manager.h
@@ -146,9 +146,17 @@ class PropertyMessageManager : public grape::DefaultMessageManager {
   }
   // for java
   template <typename GRAPH_T, typename VERTEX_T, typename MESSAGE_T>
-  inline bool GetMessage(const GRAPH_T& frag, typename VERTEX_T& v,
-                         MESSAGE_T& msg) {
+  inline bool JavaGetMessage(const GRAPH_T& frag, typename VERTEX_T& v,
+                             MESSAGE_T& msg) {
     return GetMessage<GRAPH_T, MESSAGE_T>(frag, v, msg);
+  }
+
+  // for java
+  template <typename GRAPH_T, typename VERTEX_T, typename MESSAGE_T>
+  inline void JavaSyncStateOnOuterVertex(const GRAPH_T& frag,
+                                         const typename VERTEX_T& v,
+                                         const MESSAGE_T& msg) {
+    SyncStateOnOuterVertex<GRAPH_T, MESSAGE_T>(frag, v, msg);
   }
 };
 
