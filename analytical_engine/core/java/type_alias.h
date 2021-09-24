3283346194c002a0a723ddd5024cf9a7cf950237
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2021-09-24 15:10:06 +0800

    fix

--- analytical_engine/core/java/type_alias.h
+++ analytical_engine/core/java/type_alias.h
@@ -19,12 +19,10 @@
 #include "core/fragment/arrow_projected_fragment.h"
 #include "grape/utils/vertex_array.h"
 
+// Type alias for ease of use in Java.
 namespace gs {
 
 namespace arrow_projected_fragment_impl {
-// Type alias for ease of use in Java.
-template <typename DATA_T>
-using VertexArrayDefault = grape::VertexArray<DATA_T, uint64_t>;
 
 template <typename VID_T, typename EDATA_T>
 using NbrDefault =
@@ -34,6 +32,9 @@ template <typename VID_T, typename EDATA_T>
 using AdjListDefault =
     AdjList<VID_T, vineyard::property_graph_types::EID_TYPE, EDATA_T>;
 }  // namespace arrow_projected_fragment_impl
+
+template <typename DATA_T>
+using VertexArrayDefault = grape::VertexArray<DATA_T, uint64_t>;
 
 template <typename FRAG_T>
 using DoubleColumn = Column<FRAG_T, double>;
