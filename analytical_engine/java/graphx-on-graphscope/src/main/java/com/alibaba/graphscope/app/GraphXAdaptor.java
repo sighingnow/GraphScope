b14271913b4f381f4d0a5cb6c384008c95c0fb1e
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-03-25 17:43:24 +0800

    rename

--- analytical_engine/java/graphx-on-graphscope/src/main/java/com/alibaba/graphscope/app/GraphXAdaptor.java
+++ analytical_engine/java/graphx-on-graphscope/src/main/java/com/alibaba/graphscope/app/GraphXAdaptor.java
@@ -5,7 +5,7 @@ import com.alibaba.graphscope.context.GraphXAdaptorContext;
 import com.alibaba.graphscope.fragment.IFragment;
 import com.alibaba.graphscope.parallel.DefaultMessageManager;
 
-public class GraphxAdaptor<VDATA_T,EDATA_T> implements DefaultAppBase<Long,Long,VDATA_T,EDATA_T, GraphXAdaptorContext<VDATA_T,EDATA_T>>{
+public class GraphXAdaptor<VDATA_T,EDATA_T> implements DefaultAppBase<Long,Long,VDATA_T,EDATA_T, GraphXAdaptorContext<VDATA_T,EDATA_T>>{
 
     @Override
     public void PEval(IFragment<Long, Long, VDATA_T, EDATA_T> graph,
