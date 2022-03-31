033140fdce1ea1492557e7630dedea69ce020f97
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-03-31 09:48:11 +0800

    f

--- analytical_engine/java/graphx-on-graphscope/src/main/java/com/alibaba/graphscope/app/GraphXAdaptor.java
+++ analytical_engine/java/graphx-on-graphscope/src/main/java/com/alibaba/graphscope/app/GraphXAdaptor.java
@@ -4,9 +4,13 @@ import com.alibaba.graphscope.context.DefaultContextBase;
 import com.alibaba.graphscope.context.GraphXAdaptorContext;
 import com.alibaba.graphscope.fragment.IFragment;
 import com.alibaba.graphscope.parallel.DefaultMessageManager;
+import com.alibaba.graphscope.utils.GraphConverter;
 import com.alibaba.graphscope.utils.GraphXProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GraphXAdaptor<VDATA_T,EDATA_T> implements DefaultAppBase<Long,Long,VDATA_T,EDATA_T, GraphXAdaptorContext<VDATA_T,EDATA_T>>{
+    private static Logger logger = LoggerFactory.getLogger(GraphXAdaptor.class.getName());
 
     @Override
     public void PEval(IFragment<Long, Long, VDATA_T, EDATA_T> graph,
@@ -18,6 +22,8 @@ public class GraphXAdaptor<VDATA_T,EDATA_T> implements DefaultAppBase<Long,Long,
         //do the computation and message sending
         //flush messages.
 //        graphXProxy.compute(graph.innerVertices());
+        long tmp = GraphConverter.createArrowFragmentLoader();
+        logger.info("created fragment loader: {}", tmp);
 
         try{
             graphXProxy.invokeMain();
