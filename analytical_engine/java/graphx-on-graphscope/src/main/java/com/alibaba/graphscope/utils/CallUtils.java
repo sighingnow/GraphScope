91853cc91970ce6c35ac441d8d486ac629a43f11
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-04-07 19:39:08 +0800

    fix callUtils

--- analytical_engine/java/graphx-on-graphscope/src/main/java/com/alibaba/graphscope/utils/CallUtils.java
+++ analytical_engine/java/graphx-on-graphscope/src/main/java/com/alibaba/graphscope/utils/CallUtils.java
@@ -1,25 +1,53 @@
 package com.alibaba.graphscope.utils;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import org.checkerframework.checker.units.qual.C;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CallUtils {
+    private static Logger logger = LoggerFactory.getLogger(CallUtils.class.getName());
 
     /**
      * Get the caller class name of the class which called us.
-     * @return
+     * TODO: make this generic. now current working for UserClass -> GraphOps.Pregel -> PregeClass
+     * @return class name
      */
     public static String getCallerCallerClassName() {
         StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
         String callerClassName = null;
-        for (int i=1; i<stElements.length; i++) {
-            StackTraceElement ste = stElements[i];
-            if (!ste.getClassName().equals(CallUtils.class.getName())&& ste.getClassName().indexOf("java.lang.Thread")!=0) {
-                if (callerClassName==null) {
-                    callerClassName = ste.getClassName();
-                } else if (!callerClassName.equals(ste.getClassName())) {
-                    return ste.getClassName();
-                }
+        int index = 0;
+        while (index < stElements.length){
+            if (stElements[index].getClassName().equals(CallUtils.class.getName())){
+                break;
             }
+            index += 1;
         }
-        return null;
+        logger.info("Find CallUtils at {},total len {}", index, stElements.length);
+        while (index + 1 < stElements.length && stElements[index].getClassName().equals(stElements[index+1].getClassName())){
+            index += 1;
+        }
+        index += 1;
+        if (index >= stElements.length){
+            //we are already at last pos, so we must failed.
+            throw new IllegalStateException("already reach last " +index + " " + stElements.length + ", " +Arrays.stream(stElements).map(StackTraceElement::getClassName).collect(
+                Collectors.joining("\n")));
+        }
+        //now we are at the first position of class calling CallUtils.
+        logger.info("The class called CallUtils is " + stElements[index].getClassName());
+        int callers = 0;
+        while (index < stElements.length){
+            if (!stElements[index].getClassName().equals(stElements[index - 1].getClassName())){
+                logger.info("new caller find: " + stElements[index + 1]);
+                callers += 1;
+            }
+            if (callers == 3){
+                return stElements[index].getClassName();
+            }
+            index += 1;
+        }
+        throw new IllegalStateException("failed" + Arrays.stream(stElements).map(StackTraceElement::getClassName).collect(
+            Collectors.joining("\n")));
     }
-
 }
--- analytical_engine/java/graphx-on-graphscope/src/test/java/com/alibaba/graphscope/utils/CallUtilsTest.java
+++ analytical_engine/java/graphx-on-graphscope/src/test/java/com/alibaba/graphscope/utils/CallUtilsTest.java
@@ -4,9 +4,24 @@ package com.alibaba.graphscope.utils;
 import org.junit.Test;
 
 public class CallUtilsTest {
+    public static class A{
+        public static void a(){
+            System.out.println(CallUtils.getCallerCallerClassName());
+        }
+    }
+    public static class B{
+        public static void b(){
+            A.a();
+        }
+    }
+    public static class C{
+        public static void c(){
+            B.b();
+        }
+    }
 
+    @Test
     public void test(){
-        String val = CallUtils.getCallerCallerClassName();
-        System.out.println(val);
+        C.c();
     }
 }
