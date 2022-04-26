13277cf4686946bfed7525ee98fb9d7a262c9f8b
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-04-26 15:43:02 +0800

    f

--- analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/utils/SharedMemoryUtils.scala
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/utils/SharedMemoryUtils.scala
@@ -22,7 +22,7 @@ object SharedMemoryUtils extends Logging{
           val partition = tuple._1
           val pid = tuple._2
           val bufferedWriter = new BufferedWriter(new FileWriter(new File("/tmp/graphx-log-" + pid)))
-          val innerVerticesNum = partition.values.length
+          val innerVerticesNum = partition.size
 
           val dstFile = vprefix + pid
           val mappedBuffer = registry.mapFor(dstFile, mappedSize)
@@ -38,15 +38,16 @@ object SharedMemoryUtils extends Logging{
           mappedBuffer.writeLong(8L * innerVerticesNum)
           val vertexIter = partition.iterator
           val vidArray = new Array[Long](innerVerticesNum)
-          val vdataArray = partition.values
+          val vdataArray = new Array[VD](innerVerticesNum)
           require(vidArray.length == vdataArray.length)
           var ind = 0;
           while (vertexIter.hasNext){
             val value = vertexIter.next()
             vidArray(ind) = value._1
+            vdataArray(ind) = value._2
             ind += 1
           }
-          writeVertices(mappedBuffer, vidArray, vdataArray, innerVerticesNum, vdClass)
+          writeVertices(mappedBuffer, vidArray, vdataArray, innerVerticesNum, vdClass,bufferedWriter)
         }
       }
     )
@@ -159,7 +160,7 @@ object SharedMemoryUtils extends Logging{
     if (vdClass.equals(classOf[Long])) {
       while (ind < size) {
         buffer.writeLong(attrs(ind).asInstanceOf[Long])
-        bufferedWriter.write("write " + attrs(ind) + ",ind " + ind + " size: " + size)
+        bufferedWriter.write("write " + attrs(ind) + ",ind " + ind + " size: " + size + "\n")
         ind += 1
       }
     }
