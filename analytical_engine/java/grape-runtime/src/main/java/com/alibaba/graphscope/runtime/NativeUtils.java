fd4479d872d820b278e2f744a06c098f35d5bb78
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-05-06 11:44:49 +0800

    fix native utils

--- analytical_engine/java/grape-runtime/src/main/java/com/alibaba/graphscope/runtime/NativeUtils.java
+++ analytical_engine/java/grape-runtime/src/main/java/com/alibaba/graphscope/runtime/NativeUtils.java
@@ -1,27 +1,30 @@
 package com.alibaba.graphscope.runtime;
 
 import com.alibaba.graphscope.graphx.GrapeEdgePartition;
-import com.alibaba.graphscope.utils.CppClassName;
-import com.alibaba.graphscope.utils.array.impl.DoubleArray;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NativeUtils {
+
     private static Logger logger = LoggerFactory.getLogger(NativeUtils.class.getName());
+
     public static native long createLoader();
+
     public static native long invokeLoadingAndProjection(long addr, int vdType, int edType);
-    public static native long getArrowProjectedFragment(long fragId,String fragName);
+
+    public static native long getArrowProjectedFragment(long fragId, String fragName);
 
     public static native long nativeCreateEdgePartition(String mmFiles, long size, int edType);
 
-    public static <OID,VID,ED> GrapeEdgePartition<OID,VID,ED> createEdgePartition(String mmFiles, long size,
+    public static <OID, VID, ED> GrapeEdgePartition<OID, VID, ED> createEdgePartition(
+        String mmFiles, long size,
         Class<? extends OID> oidClass, Class<? extends VID> vidClass, Class<? extends ED> edClass)
         throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
         String foreignName = "gs::EdgePartition<int64_t,uint64_t," + clz2Str(edClass) + ">";
         long addr = nativeCreateEdgePartition(mmFiles, size, clz2Int(edClass));
-        if (addr <= 0){
+        if (addr <= 0) {
             throw new IllegalStateException("Fail to create edge partition");
         }
         return createEdgePartition(foreignName, addr);
@@ -42,31 +45,26 @@ public class NativeUtils {
         throw new IllegalStateException("No suitable constructor found");
     }
 
-    private static String clz2Str(Class<?> clz){
-        if (clz.equals(Long.class)){
+    private static String clz2Str(Class<?> clz) {
+        if (clz.equals(Long.class) || clz.getName().equals("long")) {
             return "int64_t";
-        }
-        else if (clz.equals(Double.class)){
+        } else if (clz.equals(Double.class) || clz.getName().equals("double")) {
             return "double";
-        }
-        else if (clz.equals(Integer.class)){
+        } else if (clz.equals(Integer.class) || clz.getName().equals("int")) {
             return "int32_t";
-        }
-        else {
+        } else {
             throw new IllegalStateException("Not supported ed class");
         }
     }
-    private static int clz2Int(Class<?> clz){
-        if (clz.equals(Long.class)){
+
+    private static int clz2Int(Class<?> clz) {
+        if (clz.equals(Long.class) || clz.getName().equals("long")) {
             return 0;
-        }
-        else if (clz.equals(Double.class)){
+        } else if (clz.equals(Double.class) || clz.getName().equals("double")) {
             return 1;
-        }
-        else if (clz.equals(Integer.class)){
+        } else if (clz.equals(Integer.class) || clz.getName().equals("int")) {
             return 2;
-        }
-        else {
+        } else {
             throw new IllegalStateException("Not supported ed class");
         }
     }
