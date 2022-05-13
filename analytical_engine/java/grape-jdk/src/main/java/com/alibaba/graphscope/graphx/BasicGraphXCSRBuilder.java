808d78f51fc12172a3e3edeb3463e6456ad62b68
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-05-13 12:03:05 +0800

    f

--- analytical_engine/java/grape-jdk/src/main/java/com/alibaba/graphscope/graphx/BasicGraphXCSRBuilder.java
+++ analytical_engine/java/grape-jdk/src/main/java/com/alibaba/graphscope/graphx/BasicGraphXCSRBuilder.java
@@ -1,6 +1,7 @@
 package com.alibaba.graphscope.graphx;
 
 import com.alibaba.fastffi.CXXHead;
+import com.alibaba.fastffi.FFIFactory;
 import com.alibaba.fastffi.FFIGen;
 import com.alibaba.fastffi.FFIPointer;
 import com.alibaba.fastffi.FFITypeAlias;
@@ -11,6 +12,10 @@ import com.alibaba.graphscope.utils.CppHeaderName;
 @CXXHead(CppHeaderName.CORE_JAVA_GRAPHX_GRAPHX_CSR_H)
 @CXXHead(CppHeaderName.CORE_JAVA_TYPE_ALIAS_H)
 @FFITypeAlias(CppClassName.GS_BASIC_GRAPHX_CSR_BUILDER)
-public interface BasicGraphXCSRBuilder<OID_T,VID_T> extends FFIPointer {
+public interface BasicGraphXCSRBuilder<OID_T,VID_T,ED_T> extends FFIPointer {
 
+    @FFIFactory
+    interface Factory<OID_T,VID_T,ED_T> {
+        BasicGraphXCSRBuilder<OID_T,VID_T,ED_T> create();
+    }
 }
