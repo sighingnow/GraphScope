168d9d65969f79e513210e855db6d3f1f738ef2f
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-05-18 20:34:40 +0800

    try map

--- analytical_engine/java/graphx-on-graphscope/src/main/java/com/alibaba/graphscope/utils/LongLongRecordReader.java
+++ analytical_engine/java/graphx-on-graphscope/src/main/java/com/alibaba/graphscope/utils/LongLongRecordReader.java
@@ -4,52 +4,65 @@ import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
 import java.io.IOException;
 import java.util.Iterator;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.apache.hadoop.mapreduce.lib.input.LineRecordReader;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.LineRecordReader;
+import org.apache.hadoop.mapred.RecordReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LongLongRecordReader extends RecordReader<LongWritable, LongLong> {
+public class LongLongRecordReader implements RecordReader<LongWritable, LongLong> {
 
     private Logger logger = LoggerFactory.getLogger(LongLongRecordReader.class.getName());
-    private LineRecordReader lineRecordReader = new LineRecordReader();
-    private LongLong value;
+    private LineRecordReader lineRecordReader;
+    //    private LongWritable key = new LongWritable();
+    private Text text = new Text();
 
-    @Override
-    public void initialize(InputSplit inputSplit, TaskAttemptContext taskAttemptContext)
-        throws IOException, InterruptedException {
-        lineRecordReader.initialize(inputSplit, taskAttemptContext);
+    //    private LongLong value = new LongLong();
+    public LongLongRecordReader(Configuration job, FileSplit split, byte[] recordDelimiter)
+        throws IOException {
+        lineRecordReader = new LineRecordReader(job, split, recordDelimiter);
     }
 
     @Override
-    public boolean nextKeyValue() throws IOException, InterruptedException {
-        return lineRecordReader.nextKeyValue();
+    public boolean next(LongWritable longWritable, LongLong longLong) throws IOException {
+        boolean res = lineRecordReader.next(longWritable, text);
+        if (!res) {
+            return false;
+        }
+        Iterator<String> iter = Splitter.on(CharMatcher.breakingWhitespace()).split(text.toString())
+            .iterator();
+        longLong.first = Long.parseLong(iter.next());
+        longLong.second = Long.parseLong(iter.next());
+        return true;
     }
 
     @Override
-    public LongWritable getCurrentKey() throws IOException, InterruptedException {
-        return lineRecordReader.getCurrentKey();
+    public LongWritable createKey() {
+        return new LongWritable();
     }
 
     @Override
-    public LongLong getCurrentValue() throws IOException, InterruptedException {
-        String res = lineRecordReader.getCurrentValue().toString();
-        Iterator<String> iter = Splitter.on(CharMatcher.breakingWhitespace()).split(res).iterator();
-        value.first = Long.parseLong(iter.next());
-        value.second = Long.parseLong(iter.next());
-        return value;
+    public LongLong createValue() {
+        return new LongLong();
     }
 
     @Override
-    public float getProgress() throws IOException, InterruptedException {
-        return lineRecordReader.getProgress();
+    public long getPos() throws IOException {
+        return lineRecordReader.getPos();
     }
 
     @Override
     public void close() throws IOException {
         lineRecordReader.close();
     }
+
+    @Override
+    public float getProgress() throws IOException {
+        return lineRecordReader.getProgress();
+    }
+
+
 }
