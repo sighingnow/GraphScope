5e046ea4103cb68cb33ef620770dc968e5dbdfc7
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-05-25 21:16:47 +0800

    cache vertices and edges

--- analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/impl/GrapeGraphImpl.scala
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/impl/GrapeGraphImpl.scala
@@ -37,6 +37,8 @@ class GrapeGraphImpl[VD: ClassTag, ED: ClassTag] protected(
                                                             @transient val vertices: GrapeVertexRDD[VD],
                                                             @transient val edges: GrapeEdgeRDD[ED]) extends Graph[VD, ED] with Serializable {
   val logger: Logger = LoggerFactory.getLogger(classOf[GrapeGraphImpl[_,_]].toString)
+  vertices.cache()
+  edges.cache()
 
   protected def this(vertices : GrapeVertexRDDImpl[VD], edges : GrapeEdgeRDDImpl[VD,ED], fragId : String) =
     this(vertices.asInstanceOf[GrapeVertexRDD[VD]],edges.asInstanceOf[GrapeEdgeRDD[ED]])
@@ -152,6 +154,7 @@ class GrapeGraphImpl[VD: ClassTag, ED: ClassTag] protected(
   }
   def mapVertices[VD2: ClassTag](map: (VertexId, VD) => VD2)
                                 (implicit eq: VD =:= VD2 = null): Graph[VD2, ED] = {
+    vertices.cache()
     new GrapeGraphImpl[VD2,ED](vertices.mapVertices[VD2](map), edges)
   }
   override def mapEdges[ED2: ClassTag](map: Edge[ED] => ED2): Graph[VD, ED2] = {
