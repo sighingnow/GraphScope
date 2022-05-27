ee1ee410494b260641312f56236d48e9f66bddb6
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-05-27 15:44:39 +0800

    test

---
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/SimpleTypeTest.scala
@@ -0,0 +1,25 @@
+package com.alibaba.graphscope.graphx
+
+import com.alibaba.fastffi.FFITypeFactory
+import com.alibaba.graphscope.serialization.{FFIByteVectorInputStream, FFIByteVectorOutputStream}
+import com.alibaba.graphscope.stdcxx.{FFIByteVector, FFIIntVector, FFIIntVectorFactory}
+import org.apache.spark.graphx.utils.ScalaFFIFactory
+import org.apache.spark.internal.Logging
+
+import java.io.{ObjectInputStream, ObjectOutputStream}
+
+object SimpleTypeTest extends Logging{
+  def main(args: Array[String]): Unit = {
+    System.loadLibrary("grape-jni")
+    val client = FFITypeFactory.getFactory(classOf[VineyardClient]).asInstanceOf[VineyardClient.Factory].create()
+    val ffiByteString = FFITypeFactory.newByteString()
+    ffiByteString.copyFrom("/tmp/vineyard.sock")
+    client.connect(ffiByteString)
+    log.info("vineyard connected");
+    val newVdataBuilder = ScalaFFIFactory.newVertexDataBuilder[Int]()
+    newVdataBuilder.init(62586, 1)
+    val vertexData = newVdataBuilder.seal(client).get()
+    log.info(s"Got vertexdata id ${vertexData.id()}")
+    log.info(s"get data ${vertexData.getData(0)}")
+  }
+}
