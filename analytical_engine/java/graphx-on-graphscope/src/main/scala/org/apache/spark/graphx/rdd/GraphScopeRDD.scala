77ffa73d905e321be7768d74fbeb588b9cbfa1e1
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-06-01 10:29:41 +0800

    s

--- analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/rdd/GraphScopeRDD.scala
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/rdd/GraphScopeRDD.scala
@@ -5,6 +5,7 @@ import org.apache.spark.graphx.{EdgeRDD, GrapeEdgeRDD, GrapeVertexRDD, VertexRDD
 import org.apache.spark.internal.Logging
 import org.apache.spark.scheduler.cluster.{CoarseGrainedSchedulerBackend, ExecutorData, ExecutorInfo, StandaloneSchedulerBackend}
 
+import java.net.InetAddress
 import scala.collection.mutable
 import scala.collection.mutable.HashMap
 import scala.reflect.ClassTag
@@ -33,8 +34,13 @@ object GraphScopeRDD extends Logging{
     require(field != null)
     field.setAccessible(true)
 
-    val executorHosts = field.get(castedBackend).asInstanceOf[mutable.HashMap[String,String]]
-    log.info(s"${executorHosts.values.toArray.mkString("Array(", ", ", ")")}")
-    executorHosts.values.toArray
+    val executorIps = field.get(castedBackend).asInstanceOf[mutable.HashMap[String,String]]
+    log.info(s"${executorIps.values.toArray.mkString("Array(", ", ", ")")}")
+    val executorHosts = executorIps.values.toArray.map(ip => {
+      val inetAddress = InetAddress.getByName(ip)
+      inetAddress.getHostName
+    })
+    log.info(s"transformed to hostnames: ${executorHosts.mkString("Array(", ", ", ")")}")
+    executorHosts
   }
 }
