c222e1cf969f99852671fc751b1f5d278054f158
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-06-08 11:30:28 +0800

    specify vineyard

--- analytical_engine/java/java-app-runner.py
+++ analytical_engine/java/java-app-runner.py
@@ -213,7 +213,8 @@ if __name__ == "__main__":
 
 
 import graphscope
-sess = graphscope.session(cluster_type="hosts",hosts=["d50"], num_workers=1)
+graphscope.set_option(show_log=True)
+sess = graphscope.session(cluster_type="hosts",hosts=["d50"], num_workers=1,etcd_addrs="http://11.227.236.89:2379",vineyard_socket="/tmp/vineyard.sock")
 graph = sess.g(directed=True)
 graph = graph.add_vertices("/home/graphscope/data/gstest/property/p2p-31_property_v_0","person")
 graph = graph.add_edges("/home/graphscope/data/gstest/property/p2p-31_property_e_0",label="knows",src_label="person",dst_label="person")
@@ -222,5 +223,5 @@ simple = graph_proj._project_to_simple()
 "res_str:" + simple.template_str + ";" + simple.host_ids_str
 
 
-graphscope.set_option(show_log=True)
+
 sess = graphscope.session(cluster_type="hosts", num_workers=1, hosts=["d50"], etcd_addrs="http://11.227.236.89:2379,http://11.227.236.67:2379")
