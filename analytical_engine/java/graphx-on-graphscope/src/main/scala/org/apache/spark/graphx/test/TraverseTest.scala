c50fdadf1a2c2f47826ead4d693eee7d20cf3e8b
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-06-14 15:42:56 +0800

    s

---
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/test/TraverseTest.scala
@@ -0,0 +1,36 @@
+package org.apache.spark.graphx.test
+
+import com.alibaba.graphscope.graphx.{GSSession, GraphScopeHelper}
+import org.apache.spark.graphx.impl.GrapeGraphImpl
+import org.apache.spark.graphx.rdd.GraphScopeRDD
+import org.apache.spark.graphx.test.FragmentTest.log
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.SparkSession
+
+object TraverseTest extends Logging{
+  def main(array: Array[String]) : Unit = {
+    val spark = SparkSession
+      .builder
+      .appName(s"${this.getClass.getSimpleName}")
+      .getOrCreate()
+    val sc = spark.sparkContext
+
+    val gsSession: GSSession = GraphScopeHelper.createSession(sc)
+
+    val graph: GrapeGraphImpl[Long, Long] =
+      GraphScopeRDD.loadFragmentAsGraph[Long, Long](sc,
+        "d50:250494962841990456",
+        "gs::ArrowProjectedFragment<int64_t,uint64_t,int64_t,int64_t>")
+
+    val res = graph.pregel(1, maxIterations = 10)(
+      (id, dist, newDist) => newDist,
+      triplet => { // Send Message
+        log.info(s"visiting triplet ${triplet.srcId}(${triplet.srcAttr}) -> ${triplet.dstId}(${triplet.dstAttr}), attr ${triplet.attr}")
+        Iterator.empty
+      },
+      (a, b) => math.min(a, b) // Merge Message
+    )
+    gsSession.close()
+    sc.stop()
+  }
+}
