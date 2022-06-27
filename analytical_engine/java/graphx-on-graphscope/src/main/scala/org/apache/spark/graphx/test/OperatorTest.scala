876602fab70a234b9f6d4a2ba05cfc7d0c81d26e
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-06-27 15:40:44 +0800

    s

--- analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/test/OperatorTest.scala
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/test/OperatorTest.scala
@@ -23,22 +23,22 @@ object OperatorTest extends Logging{
 
       def mapping(graph : Graph[Long,Long])  : Graph[Long,Long] = {
         graph.mapVertices((vid, vd) => vd + vid)
-          .mapEdges(edge=>edge.srcId + edge.dstId + edge.attr)
-          .mapTriplets(triplet => triplet.srcAttr + triplet.dstAttr + triplet.attr + triplet.srcId + triplet.dstId)
+          .mapEdges(edge=> {log.info(s"edge ${edge.srcId}->${edge.dstId}:${edge.attr}");edge.srcId + edge.dstId + edge.attr})
+//          .mapTriplets(triplet => triplet.srcAttr + triplet.dstAttr + triplet.attr + triplet.srcId + triplet.dstId)
       }
 
       def mapDifferentType(graph : Graph[Long,Long]) : Graph[Double,Double] = {
-        graph.mapVertices((vid, vd) => vd.toDouble).mapEdges(edge=>edge.attr.toDouble)
+        graph.mapVertices((vid, vd) => {log.info(s"${vid}(${vd})"); vd.toDouble}).mapEdges(edge=>{log.info(s"edge attr ${edge.attr}"); edge.attr.toDouble})
       }
 
       val graphxRes = mapDifferentType(mapping(graph))
 
       val grapeRes = mapDifferentType(mapping(grapeGraph))
 
-      graphxRes.vertices.saveAsTextFile(s"/tmp/operator-test-${java.time.LocalDateTime.now()}-graphx-vertex")
-      grapeRes.vertices.saveAsTextFile(s"/tmp/operator-test-${java.time.LocalDateTime.now()}-grape-vertex")
-      graphxRes.edges.saveAsTextFile(s"/tmp/operator-test-${java.time.LocalDateTime.now()}-graphx-edge")
-      grapeRes.edges.saveAsTextFile(s"/tmp/operator-test-${java.time.LocalDateTime.now()}-grape-edge")
+      graphxRes.vertices.saveAsTextFile(s"/tmp/operator-test-graphx-vertex-${java.time.LocalDateTime.now()}")
+      grapeRes.vertices.saveAsTextFile(s"/tmp/operator-test-grape-vertex-${java.time.LocalDateTime.now()}")
+      graphxRes.edges.saveAsTextFile(s"/tmp/operator-test-graphx-edge-${java.time.LocalDateTime.now()}")
+      grapeRes.edges.saveAsTextFile(s"/tmp/operator-test-grape-edge-${java.time.LocalDateTime.now()}")
       sc.stop()
     }
   }
