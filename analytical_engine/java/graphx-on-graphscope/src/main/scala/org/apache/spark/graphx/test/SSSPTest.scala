299b476331eed2e03c1b2c7f2b94c7f86d4e4068
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-06-29 16:14:27 +0800

    sssp test

---
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/test/SSSPTest.scala
@@ -0,0 +1,43 @@
+package org.apache.spark.graphx.test
+
+import org.apache.spark.graphx.{Graph, GraphLoader, VertexId}
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.SparkSession
+
+object SSSPTest extends Logging{
+  def main(args: Array[String]): Unit = {
+    // Creates a SparkSession.
+    val spark = SparkSession
+      .builder
+      .appName(s"${this.getClass.getSimpleName}")
+      .getOrCreate()
+    val sc = spark.sparkContext
+    if (args.length < 2) {
+      println("Expect 1 args")
+      return 0;
+    }
+    val efile = args(0)
+    val partNum = args(1).toInt
+    val graph: Graph[Int, Double] =
+      GraphLoader.edgeListFile(sc, efile,canonicalOrientation = false, partNum).mapEdges(e => e.attr.toDouble)
+    val sourceId: VertexId = 6 // The ultimate source
+    // Initialize the graph such that all vertices except the root have distance infinity.
+    val initialGraph = graph.mapVertices((id, _) =>
+      if (id == sourceId) 0.0 else Double.PositiveInfinity)
+    val sssp = initialGraph.pregel(Double.PositiveInfinity)(
+      (id, dist, newDist) => math.min(dist, newDist), // Vertex Program
+      triplet => {  // Send Message
+        if (triplet.srcAttr + triplet.attr < triplet.dstAttr) {
+          Iterator((triplet.dstId, triplet.srcAttr + triplet.attr))
+        } else {
+          Iterator.empty
+        }
+      },
+      (a, b) => math.min(a, b) // Merge Message
+    )
+//    println(sssp.vertices.collect.mkString("\n"))
+    sssp.vertices.saveAsTextFile(s"/tmp/sssp-test-${java.time.LocalDateTime.now()}")
+    // $example off$
+
+  }
+}
