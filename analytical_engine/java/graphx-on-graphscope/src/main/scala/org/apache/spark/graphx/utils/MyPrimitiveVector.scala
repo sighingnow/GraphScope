ccd49483826b48b119fefd8f8541b2f96f09cb1e
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-06-30 10:31:13 +0800

    fix

--- analytical_engine/java/graphx-on-graphscope/src/main/scala/org/apache/spark/graphx/utils/MyPrimitiveVector.scala
+++
@@ -1,5 +0,0 @@
-package org.apache.spark.graphx.utils
-
-class MyPrimitiveVector[T](initialSize : Int = 64) extends org.apache.spark.util.collection.PrimitiveVector[T](initialSize) with Serializable {
-
-}
