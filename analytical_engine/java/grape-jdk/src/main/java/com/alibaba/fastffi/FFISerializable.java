fdc3df127ce4c103b90eef6147b60c86a288e3d5
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-07-04 19:19:31 +0800

    s

--- analytical_engine/java/grape-jdk/src/main/java/com/alibaba/fastffi/FFISerializable.java
+++ analytical_engine/java/grape-jdk/src/main/java/com/alibaba/fastffi/FFISerializable.java
@@ -3,8 +3,9 @@ package com.alibaba.fastffi;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public interface FFISerializable extends FFIPointer,FFISettablePointer {
+public interface FFISerializable extends FFIPointer,FFISettablePointer, Serializable {
     default void readObject(ObjectInputStream aInputStream) throws IOException {
         setAddress(aInputStream.readLong());
     }
