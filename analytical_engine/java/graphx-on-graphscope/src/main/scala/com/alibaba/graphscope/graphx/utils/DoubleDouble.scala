82a3350111abaa6ffe03c31fbcf5c6df97379456
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-08-02 11:49:26 +0800

    s

--- analytical_engine/java/graphx-on-graphscope/src/main/scala/com/alibaba/graphscope/graphx/utils/DoubleDouble.scala
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/com/alibaba/graphscope/graphx/utils/DoubleDouble.scala
@@ -1,4 +1,4 @@
 package com.alibaba.graphscope.graphx.utils
 
-class DoubleDouble(var a : Double = 0.0, var b : Double= 0.0) extends Serializable{
+case class DoubleDouble(var a : Double = 0.0, var b : Double= 0.0) extends Serializable{
 }
