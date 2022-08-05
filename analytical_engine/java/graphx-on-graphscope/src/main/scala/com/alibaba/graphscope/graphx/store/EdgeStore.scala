764fb1bc0cd56d1ef2e0f95fd6f6b5692b505ef5
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-08-05 11:54:22 +0800

    s

--- analytical_engine/java/graphx-on-graphscope/src/main/scala/com/alibaba/graphscope/graphx/store/EdgeStore.scala
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/com/alibaba/graphscope/graphx/store/EdgeStore.scala
@@ -9,19 +9,24 @@ object EdgeStore extends Logging {
   val map = new java.util.HashMap[Int,LinkedBlockingQueue[AbstractDataStore[_]]]
 
   def enqueue(pid : Int, inHeapVertexDataStore: AbstractDataStore[_]) : Unit = {
-    if (!map.containsKey(pid)){
-      map.put(pid, new LinkedBlockingQueue)
-    }
+    createQueue(pid)
     val q = map.get(pid)
     q.offer(inHeapVertexDataStore)
     log.info(s"offering ${inHeapVertexDataStore} to part ${pid}")
   }
 
   def dequeue(pid : Int) : AbstractDataStore[_] = {
+    createQueue(pid)
     require(map.containsKey(pid), s"no queue available for ${pid}")
     val res = map.get(pid).take()
     log.info(s"pid ${pid} got res ${res}")
     res
+  }
+
+  def createQueue(pid : Int) : Unit = synchronized{
+    if (!map.containsKey(pid)){
+      map.put(pid, new LinkedBlockingQueue)
+    }
   }
 
 }
