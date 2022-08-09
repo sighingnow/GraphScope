837b1dfe7b470c70fde9b0cd4cd96cda87180be0
霄磊 <xiaolei.zl@alibaba-inc.com>
2022-08-09 13:50:17 +0800

    s

--- analytical_engine/java/graphx-on-graphscope/src/main/scala/com/alibaba/graphscope/graphx/test/CCTest.scala
+++ analytical_engine/java/graphx-on-graphscope/src/main/scala/com/alibaba/graphscope/graphx/test/CCTest.scala
@@ -1,6 +1,11 @@
 package com.alibaba.graphscope.graphx.test
 
 // $example on$
+import com.alibaba.graphscope.graphx.GraphScopeHelper
+import com.alibaba.graphscope.graphx.rdd.impl.VertexDataMessage
+import com.alibaba.graphscope.graphx.shuffle.EdgeShuffle
+import com.alibaba.graphscope.graphx.test.PageRankTest.log
+import com.alibaba.graphscope.graphx.utils.DoubleDouble
 import org.apache.spark.graphx.{Graph, GraphLoader}
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
@@ -27,22 +32,35 @@ object CCTest extends Logging{
       .appName(s"${this.getClass.getSimpleName}")
       .getOrCreate()
     val sc = spark.sparkContext
+    sc.getConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    sc.getConf.registerKryoClasses(Array(classOf[EdgeShuffle[_,_]], classOf[Array[Long]], classOf[Array[Int]], classOf[VertexDataMessage[_]], classOf[DoubleDouble]))
     if (args.length < 3) {
-      println("Expect 1 args")
+      println("Expect 4 args")
       return 0;
     }
     val efile = args(0)
     val partNum = args(1).toInt
-    val sourceId = args(2).toLong
-    val graph: Graph[Int, Int] =
-      GraphLoader.edgeListFile(sc, efile,canonicalOrientation = false, partNum)
+    val engine = args(2)
+    val time0 = System.nanoTime()
+    val graph: Graph[Int, Double] = {
+      if (engine.equals("gs")) {
+        GraphScopeHelper.edgeListFile(sc, efile, canonicalOrientation = false, partNum).mapEdges(e => e.attr.toDouble)
+      }
+      else if (engine.equals("graphx")){
+        GraphLoader.edgeListFile(sc, efile,canonicalOrientation = false, partNum).mapEdges(e => e.attr.toDouble)
+      }
+      else {
+        throw new IllegalStateException("gs or graphx")
+      }
+    }
+    // Initialize the graph such that all vertices except the root have distance infinity.
+    log.info(s"initial graph count ${graph.numVertices}, ${graph.numEdges}")
+    val time1 = System.nanoTime()
 
-    // Find the connected components
-    val cc = graph.connectedComponents().vertices
-    // Join the connected components with the usernames
-
-    cc.saveAsTextFile(s"/tmp/cc-test-${java.time.LocalDateTime.now()}")
-    spark.stop()
+    val cc = graph.connectedComponents()
+    log.info(s"cc graph count ${cc.numVertices}, ${cc.numEdges}")
+    val time2 = System.nanoTime()
+    log.info(s"Pregel took ${(time2 - time1)/1000000}ms, load graph ${(time1 - time0)/1000000}ms")
   }
 }
 
